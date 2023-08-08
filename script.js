3617db284ca3ad132e75796675989a30e8a9e9a9
vanysai <jaizamsf@gmail.com>
2023-08-08 17:46:50 -0300

    new sites

---
+++ script.js
@@ -0,0 +1,20 @@
+function smoothScroll(target) {
+    const element = document.querySelector(target);
+    if (element) {
+        window.scrollTo({
+            top: element.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetSection = link.getAttribute('href');
+            smoothScroll(targetSection);
+        });
+    });
+});
